23867551cbe41d5f3f213253b286c36dfd81a72d
Space-ZK <522383638@qq.com>
2017-12-17 19:44:38 +0800

    modify

--- utils/fileProcessor.js
+++ utils/fileProcessor.js
@@ -1,7 +1,3 @@
-"use strict";
-const fs = require('fs');
-
-let filePath = "Mstar_DVB.est";
-let readOptions = "utf8";
-let data = fs.readFileSync(filePath, readOptions);
-console.log(data);
+const xlsx = require("node-xlsx");
+var list = xlsx.parse("Mstar_DVB.xls");
+console.log(list);
